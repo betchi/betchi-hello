2de0ef30f29e490169d6e1ad35cdb795e9a2e513
Sugie Noriyuki <sugie.noriyuki@gmail.com>
2016-09-02 10:02:29 +0000

    delete theme.sh

--- theme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp -p node_modules/material-ui/styles/baseThemes/lightBaseTheme.js node_modules/material-ui/styles/baseThemes/lightBaseTheme.js.org
-cat node_modules/material-ui/styles/baseThemes/lightBaseTheme.js | sed 's/cyan/indigo/g' > lightBaseTheme.js
-mv -f lightBaseTheme.js node_modules/material-ui/styles/baseThemes/lightBaseTheme.js
