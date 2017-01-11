dafe6ba44be302cc39ff40ac6393f7f12e220502
Shinichi Minobe <minobe@ShinichinoMacBook-Pro.local>
2017-01-11 23:17:23 +0900

    Initial Setting

---
+++ webpack.config.dev.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: "./src/Index.tsx",
+  output: {
+    filename: "./dist/bundle.js"
+  },
+
+  devtool: "source-map",
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {loader: "ts-loader"}
+        ]
+      }
+    ]
+  },
+  performance: {
+    hints:false
+  }
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: "./src/Index.tsx",
+  output: {
+    filename: "./dist/bundle.js"
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {loader: "ts-loader"}
+        ]
+      }
+    ]
+  },
+  performance: {
+    hints:false
+  }
+};
